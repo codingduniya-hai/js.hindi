cf3de93d521cab231a493295f28f45646afd4bc9
Azeez  Khan <warsiak27@gmail.com>
2025-09-25 14:24:31 +0000

    completed the basic01

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,36 @@
+// dates
+
+let myDate  = new Date()
+//console.log(myDate.toISOString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toTimeString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toUTCString());
+//console.log(typeof myDate);re
+
+ 
+//let myCreatedDate = new Date(2023,0,22)
+
+let myCreatedDate = new Date("2023-01-14")
+//console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Date.now());
+//console.log( Math.floor(Date.now() /1000));
+
+
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{weekday:"long",month:"narrow"
+
+})
